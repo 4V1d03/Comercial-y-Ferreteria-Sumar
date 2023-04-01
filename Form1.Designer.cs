4f7b8ec0bfc06d449cdbb5f4811641d870a6d92e
Bryan Zavala <zavalarodriguez88@outlook.com>
2023-04-01 17:05:48 -0600

    color login

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,84 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtusuariolg = new System.Windows.Forms.TextBox();
-            this.txtclavelg = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtusuariolg = new TextBox();
+            txtclavelg = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(224, 29);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Login";
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(150, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(78, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Login";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(63, 144);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(59, 20);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Usuario";
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(55, 122);
+            label2.Name = "label2";
+            label2.Size = new Size(49, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Usuario";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(63, 289);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(45, 20);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Clave";
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(55, 230);
+            label3.Name = "label3";
+            label3.Size = new Size(37, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Clave";
             // 
             // txtusuariolg
             // 
-            this.txtusuariolg.Location = new System.Drawing.Point(185, 140);
-            this.txtusuariolg.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.txtusuariolg.Name = "txtusuariolg";
-            this.txtusuariolg.Size = new System.Drawing.Size(114, 27);
-            this.txtusuariolg.TabIndex = 3;
-            this.txtusuariolg.TextChanged += new System.EventHandler(this.txtusuariolg_TextChanged);
+            txtusuariolg.Location = new Point(133, 114);
+            txtusuariolg.Name = "txtusuariolg";
+            txtusuariolg.Size = new Size(149, 23);
+            txtusuariolg.TabIndex = 3;
+            txtusuariolg.TextChanged += txtusuariolg_TextChanged;
             // 
             // txtclavelg
             // 
-            this.txtclavelg.Location = new System.Drawing.Point(185, 279);
-            this.txtclavelg.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.txtclavelg.Name = "txtclavelg";
-            this.txtclavelg.Size = new System.Drawing.Size(114, 27);
-            this.txtclavelg.TabIndex = 4;
+            txtclavelg.Location = new Point(133, 227);
+            txtclavelg.Name = "txtclavelg";
+            txtclavelg.Size = new Size(149, 23);
+            txtclavelg.TabIndex = 4;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(197, 371);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(86, 31);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Ingresar";
-            this.button1.UseVisualStyleBackColor = true;
+            button1.BackColor = Color.Turquoise;
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(150, 305);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 37);
+            button1.TabIndex = 5;
+            button1.Text = "Ingresar";
+            button1.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(544, 451);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.txtclavelg);
-            this.Controls.Add(this.txtusuariolg);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(192, 255, 192);
+            ClientSize = new Size(401, 364);
+            Controls.Add(button1);
+            Controls.Add(txtclavelg);
+            Controls.Add(txtusuariolg);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "Iniciar Sesion";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
