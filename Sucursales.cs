c2e034020ecbe332f7681929ff6cc5fbfa1a7c17
4V1d03 <116779713+4V1d03@users.noreply.github.com>
2023-04-01 16:30:10 -0600

    Add files via upload

---
+++ Sucursales.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Comercial_y_Ferreteria_Sumar
+{
+    public partial class Sucursales : Form
+    {
+        public Sucursales()
+        {
+            InitializeComponent();
+        }
+    }
+}
