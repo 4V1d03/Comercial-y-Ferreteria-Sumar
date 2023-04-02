15d8f3ea825c2f35863c4d5737a9772a12e410e1
Bryan Zavala <zavalarodriguez88@outlook.com>
2023-04-01 18:01:04 -0600

    boton menu

--- Menu.Designer.cs
+++ Menu.Designer.cs
@@ -28,12 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Menu";
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(254, 114);
+            button1.Name = "button1";
+            button1.Size = new Size(228, 144);
+            button1.TabIndex = 0;
+            button1.Text = "Cerrar Sesion";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Name = "Menu";
+            Text = "Menu";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
