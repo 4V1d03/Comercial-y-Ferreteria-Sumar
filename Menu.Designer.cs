db152ce3a94ba341f58d37251e81c0b83637a32a
Bayron Aguirre Vargas <bjaguirre95@outlook.com>
2023-04-01 18:10:11 -0600

    button

--- Menu.Designer.cs
+++ Menu.Designer.cs
@@ -28,31 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            SuspendLayout();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(254, 114);
-            button1.Name = "button1";
-            button1.Size = new Size(228, 144);
-            button1.TabIndex = 0;
-            button1.Text = "Cerrar Sesion";
-            button1.UseVisualStyleBackColor = true;
+            this.button1.Location = new System.Drawing.Point(363, 190);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(326, 240);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Cerrar Sesion";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(39, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Menu
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button1);
-            Name = "Menu";
-            Text = "Menu";
-            ResumeLayout(false);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1143, 750);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Menu";
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
 
         private Button button1;
+        private Button button2;
     }
 }
